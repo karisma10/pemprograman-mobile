96af9feeaa04cea53703f3a590c08d4b2b9a0b28
karisma10 <riskakarismaaa1024@gmail.com>
2025-11-02 13:08:37 +0800

    latihan

--- tugas_4/lib/main.dart
+++ tugas_4/lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart';
+import 'package:tugas_4/page/widget_kompleks.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Contoh Navigator Flutter',
-      home: HomePage(),
+      title: 'Widget Kompleks',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pinkAccent),
+        useMaterial3: true,
+      ),
+      home: const WidgetKompleksPage(),
     );
   }
 }
